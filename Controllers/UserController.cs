a277fe57da4cc2bd3b2c44a219e8d714a2089921
Andrii Lytvyn <lytvyn349@gmail.com>
2022-08-08 00:58:41 +0300

    feat: add GET & POST methods for Notebook

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,9 @@ using System.Net.Mime;
 
 namespace MyMoneyManagerApi.Controllers;
 
+/// <summary>
+/// User API controller
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
@@ -16,9 +19,13 @@ public class UserController : ControllerBase
         _logger = logger;
     }
 
+    /// <summary>
+    /// Get User by its Id
+    /// </summary>
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Produces(MediaTypeNames.Application.Json)]
     public ActionResult<User> GetUser([FromRoute] Int64 id)
     {
         User user;
@@ -38,6 +45,54 @@ public class UserController : ControllerBase
         return user;
     }
 
+    /// <summary>
+    /// Create Notebook
+    /// </summary>
+    [HttpPost("Notebook")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public ActionResult<Notebook> CreateNotebook([FromBody] Notebook notebook)
+    {
+        using (DatabaseContext db = new())
+        {
+            db.Notebooks.Add(notebook);
+            db.SaveChanges();
+        }
+
+        return CreatedAtAction(nameof(GetNotebook), new { id = notebook.NotebookId }, notebook);
+    }
+
+    /// <summary>
+    /// Get Notebook by its Id
+    /// </summary>
+    [HttpGet("Notebook/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public ActionResult<Notebook> GetNotebook([FromRoute] Int64 id)
+    {
+        Notebook notebook;
+
+        using (DatabaseContext db = new())
+        {
+            try
+            {
+                notebook = db.Notebooks.Where(notebook => notebook.NotebookId == id).First();
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+
+        return notebook;
+    }
+
+    /// <summary>
+    /// Get Users list
+    /// </summary>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult<List<User>> GetUsers()
@@ -52,12 +107,15 @@ public class UserController : ControllerBase
         return users;
     }
 
+    /// <summary>
+    /// Create User
+    /// </summary>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [Produces(MediaTypeNames.Application.Json)]
     [Consumes(MediaTypeNames.Application.Json)]
-    public ActionResult<User> PostUser([FromBody] User user)
+    public ActionResult<User> CreateUser([FromBody] User user)
     {
         using (DatabaseContext db = new())
         {
--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 ﻿main:
-	ASPNETCORE_ENVIRONMENT=Development \
+	@ASPNETCORE_ENVIRONMENT=Development \
 	DOTNET_WATCH_RESTART_ON_RUDE_EDIT=true \
 	DOTNET_WATCH_SUPPRESS_EMOJIS=true \
 	dotnet watch
+
+ef:
+	@rm -rf Migrations
+	@dotnet ef migrations add "Test Migration"
+	@dotnet ef database drop
+	@dotnet ef database update
